df9772ef434d45a64e07a3f162376a25a1f6bc3d
HelgaNaz <onworkit@gmail.com>
2021-11-14 22:22:34 +0200

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
+console.log(galleryItems)
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+const markup = galleryItems
+    .map((item) => `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+  </div>`)
+    .join("");
+  
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+const selectImage = (event) => {
+    event.preventDefault();
+    const modalImage = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${modalImage}">
+    `);
+    instance.show();
+    
+    document.addEventListener("keydown", event => {
+        if (event.key === 'Escape') {
+            instance.close();
+            document.removeEventListener("keydown", selectImage)
+        }
+    })
+}
+gallery.addEventListener("click", selectImage);
