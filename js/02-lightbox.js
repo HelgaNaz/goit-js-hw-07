ecfb5995f09cb58bf89cec0ba78e645a5830729f
HelgaNaz <onworkit@gmail.com>
2021-11-15 13:36:27 +0200

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+const markup = galleryItems
+    .map((item) => `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>`)
+    .join("");
+  
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
+
+
